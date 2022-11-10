7f7cd1efb95d1f3a3c87abf4e40ce940a1217bd4
architasingh16 <108187931+architasingh16@users.noreply.github.com>
2022-11-10 12:57:21 -0600

    initial commit

---
+++ nov5java/src/main/java/com/itexps/utility/nov5java/Student.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itexps.utility.nov5java;
+
+/**
+ *
+ * @author archita
+ */
+public class Student {
+    private int id;
+    private String name;
+    
+     public Student() {
+    }
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+     
+    @Override
+    public String toString() {
+        return "Student{" + "id=" + id + ", name=" + name + '}';
+    }
+     
+
+    public int getId() {
+        return id;
+    }
+
+   
+
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+}
